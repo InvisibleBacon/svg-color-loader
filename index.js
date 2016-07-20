cbe2f8694e18feb6abb1c5846c03232643559588
Mark Porter <mporter@swiftsoftware.com>
2016-07-20 16:35:29 -0400

    Add ability to convert viewbox to width/height for IE9 support
    This commit takes this project away from it's original simple task, but it
    was expedient. I'll figure out a better way to structure this later

--- index.js
+++ index.js
@@ -27,7 +27,28 @@ module.exports = function(content) {
 								item.addAttr({ name: "fill", value: color, prefix: "", local: "fill" });
 						}
 					}
-				}	
+				},
+				{
+					"ViewBoxToWidthHeight": {
+						type: "perItem",
+						fn: function(item) {
+							//A lot of code here came from: https://github.com/svg/svgo/blob/master/plugins/removeViewBox.js
+							var regViewBox = /^0\s0\s([\-+]?\d*\.?\d+([eE][\-+]?\d+)?)\s([\-+]?\d*\.?\d+([eE][\-+]?\d+)?)$/;
+							if (item.isElem("svg") && item.hasAttr("viewBox")) {
+								var match = item.attr("viewBox").value.match(regViewBox);
+								if (match) {
+									var width = match[1];
+									var height = match[3];
+
+									item.addAttr({ name: "width", value: width, prefix: "", local: "width" });
+									item.addAttr({ name: "height", value: height, prefix: "", local: "height" });
+
+									item.removeAttr("viewBox");
+								}
+							}
+						}
+					}
+				}
 			]
 		}).optimize(content.toString(), function(result) {
 			callback(null, "module.exports = " + JSON.stringify("data:image/svg+xml;base64," + new Buffer(result.data).toString("base64")));
